8ac23140ae4d41b54771ee70004b8eb8d9c2dbaf
AtilaSaraiva <atilasaraiva@gmail.com>
2024-05-16 23:32:54 -0600

    adding btrfs related packages to its module

--- hosts/common/global/btrfs.nix
+++ hosts/common/global/btrfs.nix
@@ -1,6 +1,14 @@
+{ pkgs, ... }:
+
 {
   services.btrfs.autoScrub = {
     enable = true;
     interval = "monthly";
   };
+
+  environment.systemPackages = with pkgs; [
+    compsize
+    btdu
+    rmlint
+  ];
 }
