5ca51b01ff831ad387b0c354956be4cbe7902054
AtilaSaraiva <atilasaraiva@gmail.com>
2024-05-17 00:29:31 -0600

    adding a few more things for the laptop config

--- hosts/common/global/laptop.nix
+++ hosts/common/global/laptop.nix
@@ -1,9 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./network.nix
   ];
+
+
+  programs.sway.extraPackages = with pkgs; [
+    brightnessctl
+    acpi
+  ];
+
+  services.auto-cpufreq.enable = true;
 
   services.logind.lidSwitch = "suspend";
 }
