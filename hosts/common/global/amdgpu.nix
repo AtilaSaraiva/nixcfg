b01a7215d7df8cd70fb5b669ebb0302630a03437
Átila Saraiva <atilasaraiva@gmail.com>
2024-05-18 02:16:44 -0600

    for now I am disabling amdvlk

--- hosts/common/global/amdgpu.nix
+++ hosts/common/global/amdgpu.nix
@@ -1,18 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   hardware.opengl.extraPackages = with pkgs; [ # TODO: create an option for amdgpus
-    rocm-opencl-icd
-    amdvlk
+    #rocm-opencl-icd
+    #amdvlk
     vaapiVdpau
     libvdpau-va-gl
     libva
   ];
 
   hardware.opengl.extraPackages32 = [
-    pkgs.driversi686Linux.amdvlk
+    #pkgs.driversi686Linux.amdvlk
     pkgs.driversi686Linux.mesa
   ];
+
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
 
   environment.systemPackages = with pkgs; [
     radeontop
