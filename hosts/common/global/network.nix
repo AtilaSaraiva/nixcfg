d5da2abae70fe41a586b6cc62a30fd320b9b988d
Átila Saraiva <atilasaraiva@gmail.com>
2024-05-18 19:09:18 -0600

    adding kernel parameters to improve network trafic control

--- hosts/common/global/network.nix
+++ hosts/common/global/network.nix
@@ -15,4 +15,10 @@
     resolvconf.enable = true;
     useDHCP = false;
   };
+
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+
 }
