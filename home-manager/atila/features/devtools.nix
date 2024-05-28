aaee70e8f420946ad1df507468e34bcfd592a245
AtilaSaraiva <atilasaraiva@gmail.com>
2024-05-28 13:33:23 -0600

    adding mani config to devtools.nix

--- home-manager/atila/features/devtools.nix
+++ home-manager/atila/features/devtools.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   home.packages = with pkgs; [
@@ -7,4 +7,73 @@
     texliveMedium
     mani
   ];
+
+  home.file."${config.folders.projects}/mani.yaml".text = ''
+    projects:
+      simg:
+        path: julia/SeismicImagingTools.jl
+        url: git@github.com:AtilaSaraiva/SeismicImagingTools.jl.git
+        desc: Seismic inversion framework
+        tags: [ julia, seismic ]
+
+      seqcomp:
+        path: julia/SequentialCompression.jl
+        url: git@github.com:AtilaSaraiva/SequentialCompression.jl.git
+        tags: [ julia, compression ]
+
+      seismakie:
+        path: julia/SeisMakie.jl
+        url: git@github.com:AtilaSaraiva/SeisMakie.jl.git
+        tags: [ julia, plotting ]
+
+    specs:
+      custom:
+        output: table
+        parallel: true
+
+    targets:
+      all:
+        all: true
+
+    themes:
+      custom:
+        table:
+          options:
+            draw_border: true
+            separate_columns: true
+            separate_header: true
+            separate_rows: true
+
+    tasks:
+      st:
+        desc: show working tree status
+        spec: custom
+        target: all
+        theme: custom
+        cmd: git status -s
+
+      last-commit-msg:
+        desc: show last commit
+        cmd: git log -1 --pretty=%B
+
+      last-commit-date:
+        desc: show last commit date
+        cmd: |
+          git log -1 --format="%cd (%cr)" -n 1 --date=format:"%d  %b %y" \
+          | sed 's/ //'
+
+      branch:
+        desc: show current git branch
+        cmd: git rev-parse --abbrev-ref HEAD
+
+      overview:
+        desc: show branch, local and remote diffs, last commit and date
+        spec: custom
+        target: all
+        theme: custom
+        commands:
+          - task: branch
+          - task: last-commit-msg
+          - task: last-commit-date
+  '';
 }
