cc6ae8087363d1b9ef5acb56c2d43b76afef1c22
AtilaSaraiva <atilasaraiva@gmail.com>
2024-10-04 19:55:01 -0600

    wrote a configuration for the steam deck

--- flake.nix
+++ flake.nix
@@ -144,6 +144,14 @@
           ./home-manager/atila/neurus.nix
         ];
       };
+      "deck@steamdeck" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/atila/deck.nix
+        ];
+      };
     };
   };
 }
