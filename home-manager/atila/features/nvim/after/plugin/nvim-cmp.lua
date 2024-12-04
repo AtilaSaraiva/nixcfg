d0abc49af5e334be2243738e1310ae44b60b86b8
Átila Saraiva <atilasaraiva@gmail.com>
2024-12-04 05:31:33 -0700

    adding custom luasnips

--- home-manager/atila/features/nvim/after/plugin/nvim-cmp.lua
+++ home-manager/atila/features/nvim/after/plugin/nvim-cmp.lua
@@ -17,6 +17,8 @@ imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '
 smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
 
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnips/"})
+
 -- autocomplete config
 cmp.setup({
     completion = {
