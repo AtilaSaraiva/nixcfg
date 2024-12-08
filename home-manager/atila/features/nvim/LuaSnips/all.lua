1f67204b0b0711bd165e0c72671d98982c9737e7
Átila Saraiva <atilasaraiva@gmail.com>
2024-12-08 03:15:34 -0700

    removing unecessary snippet that creates hello world

--- home-manager/atila/features/nvim/LuaSnips/all.lua
+++ home-manager/atila/features/nvim/LuaSnips/all.lua
@@ -1,22 +1,2 @@
 return {
-  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
-  require("luasnip").snippet(
-  {
-      trig="hi",
-      dscr="An autotriggering snippet that expands 'hi' into 'Hello, world!",
-      regTrig=false,
-      priority=100,
-      snippetType="autosnippet"
-  },
-  {
-      t("Hello, world!"),
-  }
-  ),
-
-  -- To return multiple snippets, use one `return` statement per snippet file
-  -- and return a table of Lua snippets.
-  require("luasnip").snippet(
-    { trig = "foo" },
-    { t("Another snippet.") }
-  ),
 }
