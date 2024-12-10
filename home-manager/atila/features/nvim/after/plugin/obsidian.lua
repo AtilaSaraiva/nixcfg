f2166d4b1b98ca30c8148a9c1b4c93b2ed139019
Átila Saraiva <atilasaraiva@gmail.com>
2024-12-09 23:11:56 -0700

    adding some remaps to obsidian

--- home-manager/atila/features/nvim/after/plugin/obsidian.lua
+++ home-manager/atila/features/nvim/after/plugin/obsidian.lua
@@ -1,1 +1,7 @@
 vim.opt.conceallevel = 2
+
+vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianNew<CR>")
+
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianQuickSwitch<CR>")
+
+vim.cmd([[cnoreabbrev oo ObsidianNew]])
