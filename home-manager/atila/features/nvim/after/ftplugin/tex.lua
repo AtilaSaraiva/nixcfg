b211642944128f485bf2472e1db4dc6ea8a32b64
AtilaSaraiva <atilasaraiva@gmail.com>
2024-12-10 14:18:50 -0700

    adding a command that folds a latex environment

--- home-manager/atila/features/nvim/after/ftplugin/tex.lua
+++ home-manager/atila/features/nvim/after/ftplugin/tex.lua
@@ -1,6 +1,10 @@
 -- copies the last equation bellow
 vim.keymap.set("n", "<leader>n", "?end{equation<CR>V?begin{equation<CR>y<C-o><C-o>p")
 
+-- creates a fold for the current environment
+vim.keymap.set("n", "<leader>fl", [[/\\end{<CR>f{lyawA %}}}<Esc>?\\begin{<C-r>"}<CR>A %{{{<Esc>
+]])
+
 vim.keymap.set("n", "<leader>wc", "<cmd>VimtexCountWords<CR>")
 
 vim.keymap.set("n", "<leader>ll", "<cmd>VimtexCompile<CR>")
