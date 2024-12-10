1fa66bb779567fb2811894371fd06d8827c89fc1
Átila Saraiva <atilasaraiva@gmail.com>
2024-12-09 23:45:41 -0700

    adding mini completion

---
+++ home-manager/atila/features/nvim/lua/theHun/plugins/mini-completion.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.completion',
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets',
+        'onsails/lspkind.nvim',
+    },
+}
