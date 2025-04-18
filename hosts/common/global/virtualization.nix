35acf4f9aa4676698042615961981e04c30afb79
Átila Saraiva <atilasaraiva@gmail.com>
2025-04-17 21:06:08 -0600

    adding virtualbox

--- hosts/common/global/virtualization.nix
+++ hosts/common/global/virtualization.nix
@@ -7,5 +7,13 @@
       dockerCompat = true;
       dockerSocket.enable = true;
     };
+
+    virtualbox = {
+      guest.enable = true;
+      host = {
+        enable = true;
+        enableKvm = true;
+      };
+    };
   };
 }
