c235102a7f974fda20a7b53a7c3ad6ce17241345
AtilaSaraiva <atilasaraiva@gmail.com>
2025-05-14 13:13:27 -0600

    configuring sway for my steam deck

--- home-manager/atila/deck.nix
+++ home-manager/atila/deck.nix
@@ -27,6 +27,7 @@
     ./features/xournalpp
     ./features/tmux
     ./features/vifm
+    ./features/sway
     ./features/lf
     ./features/zsh
     ./features/feh.nix
@@ -35,6 +36,20 @@
     ./features/clitools.nix
     ./features/nixtools.nix
     ./features/defaults.nix
+  ];
+
+  displays = [
+    {
+      name = "X11-1";
+      position = "0,0";
+      mode = "1280x800";
+    }
+    {
+      name = "HDMI-A-1";
+      disable = "";
+      position = "1280,0";
+      mode = "3840x2160@60.000Hz";
+    }
   ];
 
   folders = {
@@ -46,6 +61,42 @@
   home = {
     username = "deck";
     homeDirectory = "/home/deck";
+  };
+
+  nixGL.packages = inputs.nixgl.packages;
+  nixGL.defaultWrapper = "mesa";
+
+  wayland.windowManager.sway.package = (config.lib.nixGL.wrap pkgs.sway);
+
+  home.packages = with pkgs; [
+      borg-sans-mono
+      cantarell-fonts
+      fira
+      fira-code
+      fira-code-symbols
+      font-awesome_4
+      font-awesome_5
+      noto-fonts
+      noto-fonts-cjk-sans
+      open-fonts
+      ubuntu_font_family
+      dejavu_fonts
+      freefont_ttf
+      gyre-fonts # TrueType substitutes for standard PostScript fonts
+      liberation_ttf
+      unifont
+      noto-fonts-color-emoji
+  ];
+
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Roboto" ];
+        monospace = [ "Fira Code" ];
+      };
+    };
   };
 
   programs.zsh.oh-my-zsh.theme = "kolo";
