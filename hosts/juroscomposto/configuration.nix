4cd6e8acb9b3e6e014a27b41dd69abd20626dead
Átila Saraiva <atilasaraiva@gmail.com>
2025-06-19 16:00:28 -0600

    changing locale to en_US on juroscomposto, yuck

--- hosts/juroscomposto/configuration.nix
+++ hosts/juroscomposto/configuration.nix
@@ -62,7 +62,7 @@
   networking.hostName = "juroscomposto";
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  i18n.defaultLocale = "pt_BR.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   time.timeZone = "America/Edmonton";
 
