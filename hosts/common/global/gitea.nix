bb305112ce9499b2d33a411b4e1a99b2131c6eac
Átila Saraiva <atilasaraiva@gmail.com>
2026-02-26 18:11:09 -0700

    fixing gitea config

--- hosts/common/global/gitea.nix
+++ hosts/common/global/gitea.nix
@@ -1,6 +1,8 @@
-services.gitea = {
-  enable = true;
-  database.type = "mysql";
-  port = 3000;
-  #settings.service.DISABLE_REGISTRATION = true;
-};
+{
+  services.gitea = {
+    enable = true;
+    database.type = "mysql";
+    dump.enable = true;
+    #settings.service.DISABLE_REGISTRATION = true;
+  };
+}
