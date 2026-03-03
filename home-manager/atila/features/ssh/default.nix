cb3ade28a2561f3559efaddcc1aa46fdcfa78ad3
AtilaSaraiva <atilasaraiva@gmail.com>
2026-03-03 14:22:35 -0700

    putting a file in the .ssh/config/multi folder so that the ControlMaster ssh config can do its work

--- home-manager/atila/features/ssh/default.nix
+++ home-manager/atila/features/ssh/default.nix
@@ -1,5 +1,9 @@
 {
   home.file = {
     ".ssh/config".source = ./config;
+    ".ssh/multi/README.md".text = ''
+      This folder is meant for openssh to be able
+      to reuse ssh connections in future ssh logins.
+    '';
   };
 }
