7b9f4a95c878a2ca353f50fe12048859890d3a68
Átila Saraiva <atilasaraiva@gmail.com>
2026-03-05 20:50:13 -0700

    removing global config caddy

--- hosts/common/global/caddy.nix
+++ hosts/common/global/caddy.nix
@@ -3,10 +3,6 @@
 {
   services.caddy = {
     enable = true;
-    globalConfig = ''
-      # This tells Caddy to use Tailscale for certs
-      tailscale_allow_external_config
-    '';
     virtualHosts."jellyfin.fin-shaula.ts.net".extraConfig = ''
       reverse_proxy localhost:8096
     '';
