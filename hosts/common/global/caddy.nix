ce65f0d3c62050f6b9bbd911677f2a1790ff5755
Átila Saraiva <atilasaraiva@gmail.com>
2026-03-05 21:53:45 -0700

    the reverse proxy is not really working as it should, but I will leave it here

--- hosts/common/global/caddy.nix
+++ hosts/common/global/caddy.nix
@@ -1,12 +1,24 @@
-{ ... }:
+{ pkgs, config, ... }:
 
 {
   services.caddy = {
     enable = true;
-    virtualHosts."jellyfin.fin-shaula.ts.net".extraConfig = ''
-      reverse_proxy localhost:8096
+    virtualHosts."https://jellyfin.fin-shaula.ts.net:433".extraConfig = ''
+      reverse_proxy 127.0.0.1:8096
+      '';
+    virtualHosts."localhost".extraConfig = ''
+      respond "Hello, world!"
     '';
+    package = pkgs.caddy.withPlugins {
+      plugins = [ 
+        "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" 
+      ];
+      hash = "sha256-OydhzUGG3SUNeGXAsB9nqXtnwvD36+2p3QzDtU4YyFg=";
+    };
   };
+
+  services.caddy.environmentFile = /secrets/tailscale.env;
+
 
   services.tailscale.permitCertUid = "caddy";
 }
