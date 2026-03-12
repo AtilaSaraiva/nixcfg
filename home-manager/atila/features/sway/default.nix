238122ab87e21a58ca98eda1c3c07955f9c9423a
Átila Saraiva <atilasaraiva@gmail.com>
2026-03-12 10:35:22 -0600

    replacing lf with yazi in the sway config

--- home-manager/atila/features/sway/default.nix
+++ home-manager/atila/features/sway/default.nix
@@ -493,7 +493,7 @@ in
           "${mod}+Insert" = "exec aftergameopen";
           "${mod}+Return" = "exec ${term}";
           "${mod}+t" = "exec ${term} --class dropdown_terminal -e zsh";
-          "${mod}+n" = "exec ${term} --class lf -e ${pkgs.lf}/bin/lf";
+          "${mod}+n" = "exec ${term} --class yazi -e ${pkgs.yazi}/bin/yazi";
           "${mod}+Shift+n" = "exec ${term} --class=notes ${pkgs.vifm}/bin/vifm ~/${config.folders.annex}/obsidian";
           "${mod}+Shift+q" = "kill";
           "${mod}+b" = "exec ${browser}";
