2a45eaadf2c0f69fe353fc5ce4e64d96967cd8ec
AtilaSaraiva <atilasaraiva@gmail.com>
2026-03-19 09:39:27 -0600

    fixing and updating some zshmarks

--- home-manager/atila/features/zsh/zshmarks.nix
+++ home-manager/atila/features/zsh/zshmarks.nix
@@ -9,9 +9,9 @@
   config = {
     home.file.".bookmarks".text = ''
       /tmp|tmp
-      $HOME/Files/synced/phd|phd
-      $HOME/Files/synced/phd/notes|note
-      $HOME/Files/synced/phd/classes/PHYS 580 computational physics|ta
+      $HOME/${config.folders.annex}/phd|phd
+      $HOME/${config.folders.annex}/phd/notes|note
+      $HOME/${config.folders.annex}/phd/TA/EN PH 230|ta
       $HOME/${config.folders.projects}|proj
       $HOME/${config.folders.projects}/julia|jl
       $HOME/${config.folders.repos}|repos
