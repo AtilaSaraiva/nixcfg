813a580ed485562e7ea66bfaf6ce86977c538cea
Átila Saraiva <atilasaraiva@gmail.com>
2026-05-01 16:02:53 -0600

    adding first draft of ollama configuration

---
+++ hosts/common/global/ollama.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  services.ollama = {
+    enable = true;
+    loadModels = [
+      "qwen3.6:27b-coding-mxfp8"
+    ];
+    openFirewall = false;
+    package = pkgs.ollama-cpu;
+    port = 11434;
+    syncModels = true;
+  };
+}
