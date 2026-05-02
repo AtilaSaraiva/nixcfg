3dc168644f6f22608acbad0034c76eb1aaa4a202
Átila Saraiva <atilasaraiva@gmail.com>
2026-05-02 11:12:51 -0600

    trying to fix the registry not working for other users

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,41 +1,33 @@
 { inputs, pkgs, lib, ... }:
 
 {
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    gc = {
-      automatic = true;
-      persistent = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-    extraOptions = ''
-      keep-outputs = true
-      keep-derivations = true
-    '';
-    package = pkgs.nix;
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
-      trusted-users = [ "@wheel" ];
-      allowed-users = [ "@wheel" ];
-      auto-optimise-store = true;
-      trusted-public-keys = [
-        "binarycache.com:F14RK+znP8o15IWh7ObV/gGDqif1cfddFbLHWh6BgCI="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      substituters = [
-        "https://cache.nixos.org?priority=10"
-        "https://nix-community.cachix.org"
-        "http://juroscomposto:5000?priority=1"
-      ];
-    };
-
-    # Opinionated: make flake registry and nix path match nix run ripgrep#nixpkgs defaultUserShellflake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  gc = {
+    automatic = true;
+    persistent = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+  package = pkgs.nix;
+  settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Opinionated: disable global registry
+    flake-registry = "";
+    trusted-users = [ "@wheel" ];
+    allowed-users = [ "@wheel" ];
+    auto-optimise-store = true;
+    trusted-public-keys = [
+      "binarycache.com:F14RK+znP8o15IWh7ObV/gGDqif1cfddFbLHWh6BgCI="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    substituters = [
+      "https://cache.nixos.org?priority=10"
+      "https://nix-community.cachix.org"
+      "http://juroscomposto:5000?priority=1"
+    ];
   };
 }
