6bd80666e0be789e2e95833d6eb3f5a1ac91f049
AtilaSaraiva <atilasaraiva@gmail.com>
2026-06-05 12:43:47 -0600

    changing neogit commit window to use a floating window with square borders

--- home-manager/common/features/nvim/lua/theHun/plugins/neogit.lua
+++ home-manager/common/features/nvim/lua/theHun/plugins/neogit.lua
@@ -11,5 +11,16 @@ return {
   cmd = "Neogit",
   keys = {
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
-  }
+  },
+  opts = {
+    commit_view = {
+      kind = "floating",
+    },
+    floating = {
+      relative = "editor",
+      width = 0.8,   -- 60% of screen width
+      height = 1.0,  -- 80% of screen height
+      border = "single",
+    },
+  },
 }
